dc4e16e0a2f7c3fd12cd52a5be8151a481555003
punemisal <133892419+punemisal@users.noreply.github.com>
2023-12-01 14:32:45 +0530

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay3011" # Replace with your actual S3 bucket name
+    bucket = "ajay0112" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
