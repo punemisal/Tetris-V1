d2202a58de7da0227601c408364a3a02622c6977
punemisal <133892419+punemisal@users.noreply.github.com>
2023-12-08 10:04:53 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay0112" # Replace with your actual S3 bucket name
+    bucket = "ajay0812" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
